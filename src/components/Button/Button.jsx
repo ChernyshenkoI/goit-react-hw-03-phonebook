73a5bf0688a8611eb32f42691efd638479763d5b
Iryna C <106169218+ChernyshenkoI@users.noreply.github.com>
2023-02-22 22:39:28 +0200

    added local storage

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class Button extends Component {
+  render() {
+    return <StyledBtn type="submit">Add contact</StyledBtn>;
+  }
+}
+
+export default Button;
+
+export const StyledBtn = styled.button`
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    background-color: green;
+    color: yellow;
+  }
+
+  width: 100px;
+  height: 30px;
+  font-size: 14px;
+  font-weight: bold;
+  color:blue;
+  background-color:yellow;
+  border-radius: 5px;
+  border-color: blue;
+
+`;
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+class Title extends Component {
+  render() {
+    const { title } = this.props;
+    return <StyledTitle>{title}</StyledTitle>;
+  }
+}
+
+export default Title;
+
+const StyledTitle = styled.h2`
+  margin-bottom: 20px;
+  color: blue;
+`;
+
+Title.propTypes = {
+  title: PropTypes.string,
+
+};
